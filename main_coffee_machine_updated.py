3187340c55c6916b87d3cb830850e15d8babe3b6
semilogoolusola <semilogoolusola@ymail.com>
2025-02-26 11:38:22 +0100

    Updated code

---
+++ main_coffee_machine_updated.py
@@ -0,0 +1,99 @@
+class CoffeeMaker:
+    """Models the machine that makes the coffee"""
+    def __init__(self):
+        self.resources = {
+            "water": 300,
+            "milk": 200,
+            "coffee": 100,
+        }
+
+    def report(self):
+        """Prints a report of all resources."""
+        print(f"Water: {self.resources['water']}ml")
+        print(f"Milk: {self.resources['milk']}ml")
+        print(f"Coffee: {self.resources['coffee']}g")
+
+    def is_resource_sufficient(self, drink):
+        """Returns True when order can be made, False if ingredients are insufficient."""
+        can_make = True
+        for item in drink.ingredients:
+            if drink.ingredients[item] > self.resources[item]:
+                print(f"Sorry there is not enough {item}.")
+                can_make = False
+        return can_make
+
+    def make_coffee(self, order):
+        """Deducts the required ingredients from the resources."""
+        for item in order.ingredients:
+            self.resources[item] -= order.ingredients[item]
+        print(f"Here is your {order.name} ☕️. Enjoy!")
+
+
+
+class MoneyMachine:
+    CURRENCY = "$"
+    COIN_VALUES = {
+        "quarters": 0.25,
+        "dimes": 0.10,
+        "nickles": 0.05,
+        "pennies": 0.01
+    }
+
+    def __init__(self):
+        self.profit = 0
+        self.money_received = 0
+
+    def report(self):
+        """Prints the current profit"""
+        print(f"Money: {self.CURRENCY}{self.profit}")
+
+    def process_coins(self):
+        """Returns the total calculated from coins inserted."""
+        print("Please insert coins.")
+        for coin in self.COIN_VALUES:
+            self.money_received += int(input(f"How many {coin}?: ")) * self.COIN_VALUES[coin]
+        return self.money_received
+
+    def make_payment(self, cost):
+        """Returns True when payment is accepted, or False if insufficient."""
+        self.process_coins()
+        if self.money_received >= cost:
+            change = round(self.money_received - cost, 2)
+            print(f"Here is {self.CURRENCY}{change} in change.")
+            self.profit += cost
+            self.money_received = 0
+            return True
+        else:
+            print("Sorry that's not enough money. Money refunded.")
+            self.money_received = 0
+            return False
+
+
+
+from coffee_maker import CoffeeMaker
+# coffee_maker= CoffeeMaker().report()
+# print(coffee_maker)
+
+from  money_machine import MoneyMachine
+# money_machine=MoneyMachine().report()
+# print(money_machine)
+
+from menu import Menu,MenuItem
+ON=True
+while ON:
+    buyer=input(f"Type what you want to do: report/off/ {Menu().get_items()} ")
+    # print(buyer)
+
+    if buyer=="report":
+        CoffeeMaker().report()
+        MoneyMachine().report()
+    elif buyer=="off":
+        ON=False
+        print("Machine goes off now!")
+    else:
+        buyer_order = Menu().find_drink(buyer)
+        if  CoffeeMaker().is_resource_sufficient(buyer_order):
+            if  MoneyMachine().make_payment(buyer_order.cost):
+                CoffeeMaker().make_coffee(buyer_order)
+
+
